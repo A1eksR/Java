29eaab46ba4231be296f9959607a87bcd26aac32
A1eksR <147248309+A1eksR@users.noreply.github.com>
2024-07-27 13:50:07 +0300

    Md2

--- Day2Md/src/main/java/org/example/Main.java
+++ Day2Md/src/main/java/org/example/Main.java
@@ -10,20 +10,27 @@ public class Main {
         List<Integer> list = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         int skaitlis = 0;
-        for(int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             System.out.println("Ievadi skaitli");
             skaitlis = sc.nextInt();
             list.add(skaitlis);
         }
+    }
+    public static List apstradatListu(List<Integer> list){
+        Scanner sc = new Scanner(System.in);
         int jaunaisSkaitlis;
         for(int i = 0; i < list.size(); i++){
             if(list.get(i) == 0){
                 System.out.println("Ievadi citu skaitli jo sis ir 0");
                 jaunaisSkaitlis = sc.nextInt();
-               list.get(i) = jaunaisSkaitlis;
+                list.set(i, jaunaisSkaitlis);
             }
         }
-        System.out.println(list);
-
+        return list;
+    }
+    public static void izvaditListu(List<Integer> list){
+        for(int i =0; i < list.size(); i++){
+            System.out.print(list.get(i));
+        }
     }
 }
