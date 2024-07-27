1bed570d9555afcfbc071b4a8cd27d60c05aca5a
A1eksR <147248309+A1eksR@users.noreply.github.com>
2024-07-27 14:45:02 +0300

    Day3v2

--- Day3/src/main/java/org/example/Main.java
+++ Day3/src/main/java/org/example/Main.java
@@ -7,31 +7,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ievadi skaitli cik garu sarakstu gribi taisit!");
-        Scanner sc = new Scanner(System.in);
-        int listGarums = sc.nextInt();
+        try{
+            System.out.println("Ievadi skaitli cik garu sarakstu gribi taisit!");
+            Scanner sc = new Scanner(System.in);
+            int listGarums = sc.nextInt();
 
-        List<Integer> list = new ArrayList();
-        int skaitlis = 0;
-        for (int i = 0; i < listGarums; i++) {
-            System.out.println("Ievadi skaitli!");
-            skaitlis = sc.nextInt();
-        }
-        apstradatList(list);
-        izvaditList(list);
-    }
-    public static void apstradatList(List<Integer> list){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ievadi pirmo skaitli no lista ko gribi dzest!");
-        int dzestSkaitli1 = sc.nextInt();
-
-        System.out.println("Ievadi otro skaitli no lista ko gribi dzest!");
-        int dzestSkaitli2 = sc.nextInt();
-
-        for(int i = 0; i < list.size(); i++){
-            if(list.get(i) == dzestSkaitli1 || list.get(i) == dzestSkaitli2){
-                list.remove(i);
+            List<Integer> list = new ArrayList();
+            int skaitlis = 0;
+            for (int i = 0; i < listGarums; i++) {
+                System.out.println("Ievadi skaitli!");
+                skaitlis = sc.nextInt();
             }
+            apstradatList(list);
+            izvaditList(list);
+        }
+        catch(Exception e){
+            System.out.println("Nepareiza Ievade");
+        }
+    }
+    public static void apstradatList(List<Integer> list) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Ievadi pirmo skaitli no lista ko gribi dzest!");
+            int dzestSkaitli1 = sc.nextInt();
+
+            System.out.println("Ievadi otro skaitli no lista ko gribi dzest!");
+            int dzestSkaitli2 = sc.nextInt();
+
+            if (list.get(dzestSkaitli1) != null) {
+                list.remove(dzestSkaitli1);
+            }
+            if (list.get(dzestSkaitli2) != null) {
+                list.remove(dzestSkaitli2);
+            }
+        } catch (Exception e) {
+            System.out.println("Nepareiza Ievade");
         }
     }
     public static void izvaditList(List<Integer> list){
