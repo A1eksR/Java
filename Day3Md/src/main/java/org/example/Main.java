25ded012167b80be9fd237882c32491a55ae2d90
A1eksR <147248309+A1eksR@users.noreply.github.com>
2024-08-10 13:14:32 +0300

    Day3Mdv2

--- Day3Md/src/main/java/org/example/Main.java
+++ Day3Md/src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -8,25 +11,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ievadi cik garu gribi Listu!");
-        Scanner sc = new Scanner(System.in);
-        int garums = sc.nextInt();
-        List<String> list = new ArrayList<>();
-        String ievade = "";
-        for(int i = 0; i < garums; i++){
-            ievade = sc.nextLine();
-            list.add(ievade);
-        }
-        try {
-            FileWriter myWriter = new FileWriter("filename.txt");
-            for(int i = 0; i < list.size(); i++){
-                myWriter.write(list);
+        try{
+            System.out.println("Ievadi cik garu gribi Listu!");
+            Scanner sc = new Scanner(System.in);
+            int garums = sc.nextInt();
+            List<String> list = new ArrayList<>();
+
+            for(int i = 0; i < garums + 1; i++){
+                System.out.println("Ievadi lista elementu");
+                String ievade = sc.nextLine();
+                list.add(ievade);
             }
-            myWriter.close();
-            System.out.println("Successfully wrote to the file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+
+            try {
+                FileWriter myWriter = new FileWriter("filename.txt");
+                for(int i = 0; i < list.size(); i++){
+                    myWriter.write(list.get(i) + "\n");
+                }
+                myWriter.close();
+                System.out.println("Successfully wrote to the file.");
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+            try {
+                File myObj = new File("filename.txt");
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    System.out.println("Fila elementi: " + data);
+                }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error");
         }
     }
 }
